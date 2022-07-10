82abdeed60203664a3a4b1d330278d2511cb2c50
abdualmajeedhamzh <abdualmajeedhamzh@gmail.com>
2022-07-10 22:02:13 +0300

    connect_to_firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    // Replace with actual values
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyAuLMouH-qQS69Bg3M3speNtuFOdisODWA",
+      authDomain: "teacher-8c9f1.firebaseapp.com",
+      projectId: "teacher-8c9f1",
+      storageBucket: "teacher-8c9f1.appspot.com",
+      messagingSenderId: "223768088640",
+      appId: "1:223768088640:web:f294421a7c6d68bb68cfa5",
+    ),
+  );
   runApp(MyApp());
 }
 
